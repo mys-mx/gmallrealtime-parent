ea64cb24ac198b53438319f7c2e4f4fc74cc69d5
13373338337@163.com <13373338337@163.com>
2021-09-11 14:15:47 +0800

    java 调用 python算法

---
+++ javaquotepython/src/main/java/com/example/javaquotepython/JavaquotepythonApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaquotepython;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaquotepythonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaquotepythonApplication.class, args);
+    }
+
+}
