95b9f156291fe4d21224b929bb536d9b85080117
amos <13373338337@163.com>
2022-02-27 19:09:48 +0800

    1.mysql配置文件类

---
+++ gmallAmos-realtime/src/main/java/com/amos/gmall/realtime/common/MysqlPropertiesConstants.java
@@ -0,0 +1,17 @@
+package com.amos.gmall.realtime.common;
+
+/**
+ *
+ * @author amos
+ * @date 2022/02/27 17:24
+ */
+public class MysqlPropertiesConstants {
+
+    /**
+     * 反位 配置文件地址
+     */
+    public static final String MYSQL_PROPERTIES_FILE_NAME = "/mysql.properties";
+
+
+
+}
