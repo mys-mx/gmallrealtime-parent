5eae1e251e804367c4138ec2397cbbfff25806ee
amos <13373338337@163.com>
2022-02-27 19:08:01 +0800

    1.公共配置类

---
+++ gmallAmos-realtime/src/main/java/com/amos/gmall/realtime/common/CommonPropertiesConstants.java
@@ -0,0 +1,57 @@
+package com.amos.gmall.realtime.common;
+
+
+/**
+ * @date 2021/4/18 17:24
+ */
+public class CommonPropertiesConstants {
+    /**
+     * Kafka 消费组id
+     */
+    public static final String PROPERTIES_FILE_NAME ="";
+
+
+    /**
+     * Kafka 消费组id
+     */
+    public static final String KAFKA_GROUP_ID = "kafka.group.id";
+    /**
+     * Kafka 集群地址
+     */
+    public static final String KAFKA_BROKERS = "kafka.brokers";
+
+    /**
+     * 默认数据发送kafka 的topic
+     */
+    public static final String DEFAULT_DATA = "default.data.topic";
+    /**
+     * 状态后端集群地址
+     */
+    public static final String FS_STATE_BACKEND_ADDRESS = "fs.state.backend.address";
+
+    /**
+     * mysql host
+     */
+    public static final String MYSQL_HOST = "mysql.host";
+    /**
+     * mysql 用户
+     */
+    public static final String MYSQL_USER = "mysql.user";
+    /**
+     * mysql 密码
+     */
+    public static final String MYSQL_PASSWORD = "mysql.password";
+    /**
+     * mysql 端口
+     */
+    public static final String MYSQL_PORT = "mysql.port";
+    /**
+     * mysql 数据库
+     */
+    public static final String MYSQL_DATABASES = "mysql.databases";
+
+    /**
+     * mysql 表
+     */
+    public static final String MYSQL_TABLES = "mysql.tables";
+}
