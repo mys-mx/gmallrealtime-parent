f34d0b44e8d5c2e6d859c2e4dcd85c2a5fdacd74
amos <13373338337@163.com>
2022-02-27 19:10:08 +0800

    1.配置文件常量定义

---
+++ gmallAmos-realtime/src/main/java/com/amos/gmall/realtime/common/PropertiesConstants.java
@@ -0,0 +1,17 @@
+package com.amos.gmall.realtime.common;
+
+/**
+ * 配置文件常量定义
+ * @author wdl
+ * @date 2021/4/18 17:24
+ */
+public class PropertiesConstants {
+
+    /*---------------------公共配置-----------------------*/
+    /**
+     * 配置文件地址
+     */
+    public static final String STREAM_CHECKPOINT_ENABLE = "stream.checkpoint.enable";
+    public static final String STREAM_CHECKPOINT_INTERVAL = "stream.checkpoint.interval";
+
+}
