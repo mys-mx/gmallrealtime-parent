0191cd7ac56ef30a9eb6ffbb00fd34fb03d282ac
牟玉嵩 <13373338337@163.com>
2022-03-28 23:06:29 +0800

    1.iceberg-spark

---
+++ iceberg-spark/src/main/scala/com/amos/SparkIceberg.scala
@@ -0,0 +1,35 @@
+package com.amos
+
+import org.apache.spark.sql.SparkSession
+
+
+/**
+ * SparkSQL 与 Iceberg整合
+ */
+object SparkIceberg {
+  def main(args: Array[String]): Unit = {
+
+    //创建Spark Catalog
+    val spark: SparkSession = SparkSession.builder().master("local")
+      .appName("SparkOperateIceberg")
+      // 设置 hive catalog
+      .config("spark.sql.catalog.hive_prod", "org.apache.iceberg.spark.SparkCatalog")
+      .config("spark.sql.catalog.hive_prod.type", "hive")
+      .config("spark.sql.catalog.hive_prod.uri", "thrift://hadoop01:9083")
+      .config("iceberg.engine.hive.enabled", "true")
+
+      //设置hadoop catalog
+      .config("spark.sql.catalog.hadoop_prod", "org.apache.iceberg.spark.SparkCatalog")
+      .config("spark.sql.catalog.hadoop_prod.type", "hadoop")
+      .config("spark.sql.catalog.hadoop_prod.warehouse", "hdfs://hadoop01:8020/sparkoperateiceberg")
+      .getOrCreate()
+
+      spark.sql(
+        """
+          |create table if not exists hive_prod.default.test(id int,name string,age int ) using iceberg
+          |""".stripMargin)
+
+    spark.stop()
+  }
+
+}
