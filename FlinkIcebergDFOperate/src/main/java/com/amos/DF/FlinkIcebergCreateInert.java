b31a6f7cbd5110fc99fc4babd63d6c6c22bf0ce1
牟玉嵩 <13373338337@163.com>
2022-04-01 14:39:16 +0800

    1.flink(dataframe)-iceberg kafka-->iceberg

--- FlinkIcebergDFOperate/src/main/java/com/amos/DF/FlinkIcebergCreateInert.java
+++ FlinkIcebergDFOperate/src/main/java/com/amos/DF/FlinkIcebergCreateInert.java
@@ -12,10 +12,12 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.data.GenericRowData;
 import org.apache.flink.table.data.RowData;
 import org.apache.flink.table.data.StringData;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.*;
+import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.flink.TableLoader;
 import org.apache.iceberg.flink.sink.FlinkSink;
-import org.apache.iceberg.hadoop.HadoopTables;
+import org.apache.iceberg.hadoop.HadoopCatalog;
 import org.apache.iceberg.types.Types;
 
 import java.util.Map;
@@ -28,11 +30,11 @@ import java.util.Map;
 public class FlinkIcebergCreateInert {
 
     // 4 Hadoop集群
-    private static final String basePath = "hdfs://hadoop-slave2:6020/";
-    private static final String tablePath = basePath.concat("warehouse/iceberg/sensordata");
+    private static final String basePath = "hdfs://hadoop-slave2:6020/warehouse/iceberg/";
+    private static final String tablePath = basePath.concat("hadoop/sensordata");
     private static final String BOOTS_SERVER = "node187:9092,hadoop-slave2:9092,node186:9092";
     private static final String TOPIC = "flink-iceberg-topic";
-    private static final String GROUPID = "flink-group-id";
+    private static final String GROUPID = "flink-group-id1";
     private static final String SOURCE_NAME = "kafka_source";
 
     public static void main(String[] args) throws Exception {
@@ -40,7 +42,6 @@ public class FlinkIcebergCreateInert {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         // 设置checkpoint
         env.enableCheckpointing(5000);
-
         //Flink读取kafka中的数据配置
         KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
                 .setBootstrapServers(BOOTS_SERVER)
@@ -49,11 +50,9 @@ public class FlinkIcebergCreateInert {
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .setStartingOffsets(OffsetsInitializer.latest())
                 .build();
-
-        // 2 加载数据源
+        //加载kafka中数据
         DataStreamSource<String> inputStream = env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), SOURCE_NAME);
-
-
+        //数据转换
         SingleOutputStreamOperator<RowData> kafkaDS = inputStream.map(new MapFunction<String, RowData>() {
             @Override
             public RowData map(String line) throws Exception {
@@ -67,35 +66,30 @@ public class FlinkIcebergCreateInert {
             }
         });
 
-        // 5 设置数据的存储格式：orc、parquet或者avro
-        Map<String, String> props =
-                ImmutableMap.of(TableProperties.DEFAULT_FILE_FORMAT, FileFormat.PARQUET.name());
-
-        // 3 设置Hive表的Schema信息
+        // 通过flink sql创建iceberg表
+        Configuration conf = new Configuration();
+        HadoopCatalog hpCatalog = new HadoopCatalog(conf, basePath);
+        TableIdentifier name = TableIdentifier.of("hadoop", "sensordata");
+        // 不设置分区
+        PartitionSpec spec = PartitionSpec.unpartitioned();
+        // 构建schemal信息
         Schema schema = new Schema(
                 Types.NestedField.required(1, "id", Types.IntegerType.get()),
                 Types.NestedField.required(2, "name", Types.StringType.get()),
                 Types.NestedField.required(3, "age", Types.IntegerType.get()),
                 Types.NestedField.required(4, "loc", Types.StringType.get()));
-        HadoopTables hadoopTables = new HadoopTables();
-        //不设置分区
-        PartitionSpec spec = PartitionSpec.unpartitioned();
-        Table table = null;
-        if (!hadoopTables.exists(tablePath)) {
-            table = hadoopTables.create(schema, spec, props, tablePath);
-        } else {
-            table = hadoopTables.load(tablePath);
+        Map<String, String> props =
+                ImmutableMap.of(TableProperties.DEFAULT_FILE_FORMAT, FileFormat.PARQUET.name());
+        //表是否存在 ，不存在创建
+        if (!hpCatalog.tableExists(name)) {
+            hpCatalog.createTable(name, schema, spec, props);
         }
+        TableLoader tableLoader = TableLoader.fromHadoopTable(tablePath, conf);
 
-        TableLoader tableLoader = TableLoader.fromHadoopTable(tablePath);
-        kafkaDS.print();
-        // 6 向Iceberg的hdfs的目录写入数据
         FlinkSink.forRowData(kafkaDS)
-                .table(table)
                 .tableLoader(tableLoader)
                 .overwrite(false)//false不覆盖数据
                 .build();
-
         env.execute(" flink 向 Iceberg 中写入数据");
     }
 }
